13998612b1c404af5face290bde893bd77a826ed
unknown <smdemon3@gmail.com>
2024-03-07 19:37:18 +0600

    create first obj and get

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('object');
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async() => {
+    const result = await prisma.post.create({
+        data: {
+            title: 'Ttitle 2',
+            content: "content 2",
+            authorName: 'EMon 2'
+        }
+    })
+   const get = await prisma.post.findMany();
+   console.log(get);
+}
+
+main()
