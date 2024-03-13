14710cef9a7caeac561baa7bd1944af921033bb9
unknown <smdemon3@gmail.com>
2024-03-13 12:05:30 +0600

    implement raw query in prisma

---
+++ src/raw-query.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const rawQuery = async () => {
+  const posts = await prisma.$queryRaw`SELECT * FROM "posts"`;
+  console.log(posts);
+};
+
+rawQuery();
